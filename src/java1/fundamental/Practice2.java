5142c7c97cf4e60aa13b45aeda009e091db08a6c
Vitaliy <vitalii.grebnev@mail.ru>
2018-11-09 20:15:30 +0400

    add Practice 3,4,5;

--- src/java1/fundamental/Practice2.java
+++ src/java1/fundamental/Practice2.java
@@ -1,6 +1,5 @@
 package java1.fundamental;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
@@ -8,7 +7,7 @@ import java.util.Random;
 public class Practice2 {
     private static int sizeMass;
     private static int num;
-    private static int [] mass;
+    private static int[] mass;
 
 
     public Practice2() {
@@ -19,35 +18,38 @@ public class Practice2 {
         System.out.println("Введите число для поиска: ");
         num = in.nextInt();
         mass = new int[sizeMass];
-        for(int i = 0; i<sizeMass; i++)mass[i] = random.nextInt(100);
+        for (int i = 0; i < sizeMass; i++) mass[i] = random.nextInt(100);
 
     }
-    public void showMass(){
-        for(int i = 0; i<sizeMass; i++)System.out.print(mass[i]+" ");
+
+    public void showMass() {
+        for (int i = 0; i < sizeMass; i++) System.out.print(mass[i] + " ");
         System.out.println();
     }
-    public void searchOneByOne(){
+
+    public void searchOneByOne() {
         long startTime = System.nanoTime();
 
-        boolean bool= false;
-        for(int i = 0; i<sizeMass; i++){
-            if(mass[i]==num){
-                bool=true;
+        boolean bool = false;
+        for (int i = 0; i < sizeMass; i++) {
+            if (mass[i] == num) {
+                bool = true;
                 break;
             }
         }
-        long endTime   = System.nanoTime();
+        long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
-        System.out.println("Cодержит ли массив число(способ OneByOne): "+(bool?"true;":"false;")+" time is "+totalTime);
+        System.out.println("Cодержит ли массив число(способ OneByOne): " + (bool ? "true;" : "false;") + " time is " + totalTime);
     }
-    public void searchBinary(){
+
+    public void searchBinary() {
         long startTime = System.nanoTime();
         Arrays.sort(mass);
         int i = -1;
         if (this.mass != null) {
             int low = 0, high = this.mass.length, mid;
             while (low < high) {
-                mid = (low + high)/2; // Можно заменить на: (low + high) >>> 1, чтоб не возникло переполнение целого
+                mid = (low + high) / 2; // Можно заменить на: (low + high) >>> 1, чтоб не возникло переполнение целого
                 if (num == this.mass[mid]) {
                     i = mid;
                     break;
@@ -60,8 +62,8 @@ public class Practice2 {
                 }
             }
         }
-        long endTime   = System.nanoTime();
+        long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
-        System.out.println("Cодержит ли массив число(способ binary): "+((i==-1)?"false;":"true;")+" time is "+totalTime);
+        System.out.println("Cодержит ли массив число(способ binary): " + ((i == -1) ? "false;" : "true;") + " time is " + totalTime);
     }
 }
---
+++ src/java1/fundamental/Practice5TowersOfHanoi.java
@@ -0,0 +1,14 @@
+package java1.fundamental;
+
+public class Practice5TowersOfHanoi {
+    public void solve(int n, String start, String auxiliary, String end) {
+        if (n == 1) {
+            System.out.println(n + " moved from " + start + " to " + end);
+        } else {
+            solve(n - 1, start, end, auxiliary);
+            System.out.println(n + " moved from " + start + " to " + end);
+            solve(n - 1, auxiliary, start, end);
+        }
+    }
+
+}
