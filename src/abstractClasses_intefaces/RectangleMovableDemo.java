8d367ccd9a8548f0c56728497d4911a9408be296
Vitaliy <vitalii.grebnev@mail.ru>
2018-11-20 17:06:11 +0400

    add some printable

--- src/abstractClasses_intefaces/RectangleMovableDemo.java
+++ src/abstractClasses_intefaces/RectangleMovableDemo.java
@@ -8,17 +8,17 @@ public class RectangleMovableDemo {
         RectangleMovable rectangleMovable = new RectangleMovable(topLeft, bottomRight);
         System.out.println(rectangleMovable);
         rectangleMovable.moveUp();
-        System.out.println(rectangleMovable);
+        System.out.println("moveUp "+rectangleMovable);
         rectangleMovable.moveDown();
-        System.out.println(rectangleMovable);
+        System.out.println("moveDown "+rectangleMovable);
         rectangleMovable.moveDown();
-        System.out.println(rectangleMovable);
+        System.out.println("moveDown "+rectangleMovable);
         rectangleMovable.moveDown();
-        System.out.println(rectangleMovable);
+        System.out.println("moveDown "+rectangleMovable);
         rectangleMovable.moveLeft();
-        System.out.println(rectangleMovable);
+        System.out.println("moveLeft "+rectangleMovable);
         rectangleMovable.moveRight();
-        System.out.println(rectangleMovable);
+        System.out.println("moveRight "+rectangleMovable);
         System.out.println(topLeft);
         System.out.println(bottomRight);
     }
